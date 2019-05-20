12106febdcaa29c4966a1543f41cc137173152e9
juaanhs <ruaanhs@gmail.com>
2019-05-19 21:13:19 -0300

    cardview layout viagens

---
+++ app/src/main/java/br/com/juaanhs/viagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package br.com.juaanhs.viagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.juaanhs.viagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
